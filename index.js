a0fee8157911acb28363aa6c112f395b0f5e1af9
vanderkilu <vanderkiliku@gmail.com>
2019-08-23 00:02:30 +0000

    add event listener for transition end

--- index.js
+++ index.js
@@ -9,12 +9,12 @@
             this.left = 0
             this.create()
             this.setInitialPosition()
-            
+            this.attachListeners()          
         }
         create() {
             const word = document.createElement('div')
             word.className = 'word'
-            const letters = this.wordText.split('').map(letter => {
+            const letters = this.wordText.split().map(letter => {
                 return `<span class="letter">${letter}</span>`
             })
             word.innerHTML = letters
@@ -33,23 +33,41 @@
             this.word.style.left = randomX + 'px'
         }
         move() {
-            //generate a randomSpeed between 2 and 5
-            const randomSpeed = Math.floor(Math.random() * 5) + 2
+            //generate a randomSpeed between 3 and 6
+            const randomSpeed = Math.floor(Math.random() * 6) + 3
             this.word.style.transition = `all ${randomSpeed}s linear`
             this.word.classList.add('active')
+        }
+        attachListeners() {
+            this.word.addEventListener('transitionend', this.onLand.bind(this))
+        }
+        destroy() {
+            this.word.parentNode.removeChild(this.word)
+        }
+        onLand() {
+            this.destroy()
         }
 
     }
 
     class GameWorld {
         constructor() {
-            this.wordMoveTimer = null
-            this.w = new Word('mango')
-            // this.words = ['mango', 'apple', 'banana', 'orange']
-            // this.wordObj = this.words.map(w => new Word(w))
+            this.wordMovetimer = null
+            this.wordTexts = ['mango', 'apple', 'banana', 'orange']
+            this.wordObjs = this.wordTexts.map(w => new Word(w))
+        }
+        attachListeners() {
+            //
         }
         start() {
-            setTimeout(()=> this.w.move(), 300)
+            clearInterval(this.wordMoveTimer)
+            this.wordMoveTimer = setInterval(()=> {
+                const word = this.wordObjs.shift()
+                if (!word) clearInterval(this.wordMoveTimer)
+                else {
+                    word.move()
+                }
+            }, 1000)
         }
     }
 
